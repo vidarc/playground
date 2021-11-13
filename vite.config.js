0ec8cdba3c69a6c66aa691606cd4e85c305a2cf9
vidarc <mattailes@gmail.com>
2021-11-12 23:37:55 -0500

    use rollup compress plugin because it creates seperate files

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import compress from 'rollup-plugin-gzip';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
     emptyOutDir: true,
   },
   plugins: [
+    compress(),
     react({
       babel: {
         plugins: ['@compiled/babel-plugin'],
