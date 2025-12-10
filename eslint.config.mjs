8542ce1105bfc50422cbe0996a3975293787c5ff
Matthew Ailes <mattailes@gmail.com>
2025-12-09 21:00:46 -0500

    setting up the react compiler (#236)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,23 @@
 // @ts-check
 
 import eslint from '@eslint/js';
-import {defineConfig, globalIgnores} from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import tsParser from '@typescript-eslint/parser';
 import prettier from 'eslint-plugin-prettier/recommended';
-import {importX} from 'eslint-plugin-import-x';
-import playwright from 'eslint-plugin-playwright'
+import { importX } from 'eslint-plugin-import-x';
+import playwright from 'eslint-plugin-playwright';
+import reactHooks from 'eslint-plugin-react-hooks';
 
 export default defineConfig(
   globalIgnores(['dist', '.yarn', 'eslint.config.mjs', 'vite.config.js']),
   eslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
   tseslint.configs.stylisticTypeChecked,
+  // @ts-expect-error this is fine
   importX.flatConfigs.recommended,
   importX.flatConfigs.typescript,
+  reactHooks.configs.flat.recommended,
   {
     languageOptions: {
       parserOptions: {
@@ -28,5 +31,5 @@ export default defineConfig(
     files: ['tests/**'],
     extends: [playwright.configs['flat/recommended']],
   },
-    prettier,
+  prettier,
 );
